952068e59d43321e1e62ce2822205ef38e7ee5c5
Furquan Shaikh <furquan.m.shaikh@gmail.com>
2012-11-19 22:39:47 -0600

    Working code of decision tree Signed-off-by: Furquan Shaikh <furquan.m.shaikh@gmail.com>

--- fmshaik2_assign3/code/input.c
+++ fmshaik2_assign3/code/input.c
@@ -29,6 +29,11 @@ struct data *train_data, *test_data;
 int train_inst, test_inst;
 struct tree_node *dec_tree;
 int curr_attr;
+int match,mismatch;
+struct data *curr_data;
+
+char *unique_values;
+char **unique_array;
 
 unsigned int calc_number_attr(char *str)
 {
@@ -254,54 +259,44 @@ void get_class_count(struct data *data,
 	*n = countn;
 }
 
-int find_num_of_categories(struct data *data,
-			   int inst,
-			   int attr)
+int get_size(struct data *data,
+	     int inst,
+	     int attr,
+	     int val)
 {
-	int i = 0,count = 1;
+	int i;
+	int count = 0;
 
-	while (i<(inst-1)) {
-		if (data[i].attr[attr] !=
-		    data[i+1].attr[attr])
+	for (i=0; i<inst; i++) {
+		if (data[i].attr[attr] == val)
 			count++;
-		i++;
 	}
 	return count;
 }
 
-void get_unique_values(struct data *data,
-		       int inst,
-		       int attr,
-		       char *attr_split,
-		       int *part_size,
-		       int num_values)
+void get_part_size(struct data *data,
+		   int inst,
+		   int attr,
+		   double *part_size)
 {
-	int i = 0;
-	int count  = 0;
-	attr_split[count] = data[0].attr[attr];
-	part_size[count] = 0;
+	int i;
 
-	while ((count < num_values) &&
-	       (i < inst)) {
-		if (data[i].attr[attr] != attr_split[count]) {
-			count++;
-			attr_split[count] = data[i].attr[attr];
-			part_size[count] = 0;
-		}
-		part_size[count]++;
-		i++;
+	for (i=0;i<unique_values[attr];i++) {
+		part_size[i] = get_size(data,
+					inst,
+					attr,
+					unique_array[attr][i]);
+		printf("part_size:s%f\n",part_size[i]);
 	}
 }
 
 void calc_gain_ratio(struct data *data,
 		     int inst,
 		     int attr,
-		     double *attr_gain,
-		     int *num_splits)
+		     double *attr_gain)
 {
-	char *attr_split;
 	int num_values;
-	int *part_size;
+	double *part_size;
 	int i = 0;
 	double countp, countn,total,entropy=0,info_needed=0,info_gain,split_info=0,gain_ratio;
 	double *countp_part, *countn_part;
@@ -319,94 +314,159 @@ void calc_gain_ratio(struct data *data,
 		entropy += (countn/total)*log2(countn/total);
 	}
 	entropy = (-1)*entropy;
-	printf("entropy:%f\n",entropy);
+	printf("------------entropy:%f-------------\n",entropy);
 
 	/* Find number of unique values in the attribute */
-	num_values = find_num_of_categories(data,
-					    inst,
-					    attr);
+	num_values = unique_values[attr];
 
 	/* Obtain the unique values of attributes */
-	attr_split = malloc(num_values*sizeof(*attr_split));
 	part_size = malloc(num_values*sizeof(*part_size));
-	get_unique_values(data,inst,attr,attr_split,part_size,num_values);
+	get_part_size(data,inst,attr,part_size);
 
 	/* Obtain the counts of each class for each part */
 	countp_part = malloc(num_values*sizeof(*countp_part));
 	countn_part = malloc(num_values*sizeof(*countn_part));
 
 	for (i=0;i<num_values;i++) {
-		double p,n;
-		dataptr = data+inc;
-		get_class_count(dataptr,part_size[i],&p,&n);
-		countp_part[i] = p;
-		countn_part[i] = n;
-		inc += part_size[i];
+		if (part_size[i]) {
+			double p,n;
+			dataptr = data+inc;
+			get_class_count(dataptr,part_size[i],&p,&n);
+			countp_part[i] = p;
+			countn_part[i] = n;
+			inc += part_size[i];
+		}
 	}
 
 	/* Calculate info needed */
 	for (i=0;i<num_values;i++) {
-		double tmp=0;
-		printf("info_needed:%f,p:%f,n:%f\n",info_needed,countp_part[i],countn_part[i]);
-		if (countp_part[i]) {
-			tmp += countp_part[i]/part_size[i] * log2(countp_part[i]/part_size[i]);
+		if (part_size[i]) {
+			double tmp=0;
+			printf("p:%f n:%f t:%f\n",countp_part[i],
+			       countn_part[i],
+			       part_size[i]);
+			if (countp_part[i]) {
+				tmp += countp_part[i]/part_size[i] * log2(countp_part[i]/part_size[i]);
+			}
+			if (countn_part[i]) {
+				tmp +=  countn_part[i]/part_size[i] * log2(countn_part[i]/part_size[i]);
+			}
+			info_needed += (part_size[i]/total) * tmp;
 		}
-		if (countn_part[i]) {
-			tmp +=  countn_part[i]/part_size[i] * log2(countn_part[i]/part_size[i]);
-		}
-		info_needed += (part_size[i]/total) * tmp;
 	}
 
 	info_needed = (-1)*info_needed;
+	printf("info needed:%f\n",info_needed);
 
 	/* Calculate info gain */
 	info_gain = entropy - info_needed;
 
 	/* Calculate split info */
-	for (i=0;i<num_values;i++)
-		split_info += (part_size[i]/total) * log2(part_size[i]/total);
+	for (i=0;i<num_values;i++) {
+		if (part_size[i]) {
+			split_info += (part_size[i]/total) * log2(part_size[i]/total);
+		}
+	}
 	split_info = (-1) * split_info;
 	printf("split_info:%f\n",split_info);
 
 	/* Calculate gain ratio */
-	gain_ratio = info_gain / split_info;
+	if( info_gain ) {
+		gain_ratio = info_gain / split_info;
+	} else {
+		gain_ratio = 0;
+	}
 	printf("info gain:%f\n",info_gain);
 	printf("gain ratio:%f\n",gain_ratio);
 
 	*attr_gain = gain_ratio;
-	*num_splits = num_values;
 
-	free(attr_split);
 	free(part_size);
 	free(countp_part);
 	free(countn_part);
 }
 
-int attr_selection(struct data *data,
-		   int inst,
-		   int *attr)
+void attr_selection(struct data *data,
+		    int inst,
+		    int *attr)
 {
 	int i = 0;
 	double max_gain = 0, gain = 0;
-	int num_splits, max_splits = 0;
 
+	*attr = -1;
+	printf("Attribute selection\n");
 	while (i<num_attr) {
 		if(attr_bitmap & (1ULL << i)) {
-			/* Sort on a particular attribute */
-			sort_on_attr(data,inst,i);
-			/* Calculate Gain ratio */
-			calc_gain_ratio(data,inst,i,&gain,&num_splits);
-			if (gain > max_gain) {
-				max_gain = gain;
-				max_splits = num_splits;
-				*attr = i;
+			if (unique_values[i] == 1) {
+				attr_considered++;
+				attr_bitmap = attr_bitmap & (~(1ULL << i));
+			} else {
+				/* Sort on a particular attribute */
+				sort_on_attr(data,inst,i);
+				printf("calc gain on %d\n",i);
+				/* Calculate Gain ratio */
+				calc_gain_ratio(data,inst,i,&gain);
+				if (gain == 0) {
+					attr_considered++;
+					attr_bitmap = attr_bitmap & (~(1ULL << i));
+				} else if (gain > max_gain) {
+					max_gain = gain;
+					*attr = i;
+				}
 			}
 		}
 		i++;
 	}
 	attr_considered++;
-	attr_bitmap = attr_bitmap & (~(1ULL << (*attr)));
-	return max_splits;
+	if (*attr != -1) {
+		attr_bitmap = attr_bitmap & (~(1ULL << (*attr)));
+	}
+}
+
+int find_num_of_categories(struct data *data,
+			   int inst,
+			   int attr)
+{
+	int i = 0,count = 1;
+
+	while (i<(inst-1)) {
+		if (data[i].attr[attr] !=
+		    data[i+1].attr[attr])
+			count++;
+		i++;
+	}
+	return count;
+}
+
+void read_unique_array(struct data *data,
+		       int attr)
+{
+	int i = 0;
+	int count = 1;
+	int val;
+
+	val = unique_array[attr][0] = data[0].attr[attr];
+
+	while (count < unique_values[attr]) {
+		if(val != data[i].attr[attr]) {
+			val = unique_array[attr][count] = data[i].attr[attr];
+			count++;
+		}
+		i++;
+	}
+}
+
+void get_unique_values(struct data *data,
+		       int inst)
+{
+	int i;
+
+	for (i=0; i<num_attr; i++) {
+		sort_on_attr(data,inst,i);
+		unique_values[i] = find_num_of_categories(data,inst,i);
+		unique_array[i] = malloc(unique_values[i] * sizeof(*unique_array[i]));
+		read_unique_array(data,i);
+	}
 }
 
 struct tree_node *gen_decision_tree(struct data *data,
@@ -415,7 +475,7 @@ struct tree_node *gen_decision_tree(struct data *data,
 	struct node *tmp;
 	struct tree_node *node;
 	int num_split;
-	int *part_size;
+        double *part_size;
 	struct data *dataptr;
 	int inc = 0;
 	int i;
@@ -431,15 +491,17 @@ struct tree_node *gen_decision_tree(struct data *data,
 	if (data_belongs_to_same_class(data,inst)) {
 		/* Step 3: Make the node a leaf node labeled with class C
 		   and return */
+		printf("creating leaf..all same class\n");
 		tmp->label = data[0].actual_label;
 		node = create_tree_node(tmp,0);
 		return node;
 	}
 
 	/* Step 4: If attribute list is empty */
-	if (attr_considered == num_attr) {
+	if (attr_considered >= num_attr) {
 		/* Step 5: Make the node a leaf node labeled with majority
 		   class in D and return */
+		printf("no attr remaining..creating leaf\n");
 		tmp->label = find_majority_class(data, inst);
 		node = create_tree_node(tmp,0);
 		return node;
@@ -447,38 +509,107 @@ struct tree_node *gen_decision_tree(struct data *data,
 
 	/* Step 6 : Attribute selection method
 	   Step 7: Label node with split criteria */
-	num_split = attr_selection(data, inst, &tmp->attr);
+	attr_selection(data, inst, &tmp->attr);
+
+	if (tmp->attr == -1) {
+		printf("no attr remaining..creating leaf\n");
+                tmp->label = find_majority_class(data, inst);
+                node = create_tree_node(tmp,0);
+                return node;
+	}
+
+	num_split = unique_values[tmp->attr];
+
 	node = create_tree_node(tmp,num_split);
 
 	/* Sort the data on the selected attribute */
 	sort_on_attr(data,inst,tmp->attr);
 
         /* Obtain the unique values of attributes */
-        tmp->attr_split = malloc(num_split*sizeof(*(tmp->attr_split)));
+        /* tmp->attr_split = malloc(num_split*sizeof(*(tmp->attr_split))); */
+	tmp->attr_split = unique_array[tmp->attr];
         part_size = malloc(num_split*sizeof(*part_size));
-        get_unique_values(data,inst,tmp->attr,tmp->attr_split,part_size,num_split);
+	get_part_size(data,inst,tmp->attr,part_size);
 
 	printf("splitting on %d with %d splits\n",tmp->attr,num_split);
 
 	for (i=0;i<num_split;i++) {
 		dataptr = data + inc;
 		if (part_size[i]) {
+			printf("split:adding child\n");
 			add_child(node, gen_decision_tree(dataptr,part_size[i]));
 		} else {
 			struct node *new_tmp;
+			printf("split:adding leaf\n");
 			new_tmp = malloc(sizeof(*new_tmp));
 			new_tmp->label = find_majority_class(data, inst);
 			add_child(node, create_tree_node(new_tmp,0));
 		}
+		inc += part_size[i];
 	}
 
 	return node;
+}
+
+void print_unique_array()
+{
+	int i = 0,j;
+
+	while (i<num_attr) {
+		printf("Attr :%d - ",i);
+
+		for (j=0;j<unique_values[i];j++) {
+			printf(" %d ",unique_array[i][j]);
+		}
+		i++;
+		printf("\n");
+	}
+}
+
+int traverse(void *data,
+	     int num)
+{
+	int i;
+	struct node *node = (struct node*)data;
+
+	if (num == 0) {
+		printf("actual label:%d, calc_label:%d\n",curr_data->actual_label,
+		       node->label);
+		if (curr_data->actual_label == node->label)
+			match++;
+		else
+			mismatch++;
+		return -1;
+	}
+	for (i=0;i<num;i++) {
+		if (curr_data->attr[node->attr] == node->attr_split[i])
+			break;
+	}
+	if (i == num) {
+		printf("oops..nothing found\n");
+		return -1;
+	}
+	return i;
+}
+
+void calculate_label(struct data *data,
+		     int inst)
+{
+	int i;
+
+	for (i=0;i<inst;i++) {
+		curr_data = data+i;
+		traverse_path(dec_tree,
+			      traverse);
+	}
+	printf("match:%d,mismatch:%d\n",match,mismatch);
 }
 
 int main(int argc,
 	 char **argv)
 {
 	FILE *trainfp, *testfp;
+	int i;
 
 	if (argc != 3) {
 		printf("Usage:%s train_file test_file\n",argv[0]);
@@ -506,11 +637,23 @@ int main(int argc,
 	printf("%s Number of train instances:%d\n",__func__,train_inst);
 	printf("Number of test instances:%d\n",test_inst);
 
+	unique_values = malloc(num_attr*sizeof(*unique_values));
+	unique_array = malloc(num_attr*sizeof(*unique_array));
+
+	get_unique_values(train_data, train_inst);
+	print_unique_array();
+
 	dec_tree = gen_decision_tree(train_data, train_inst);
+	calculate_label(test_data,test_inst);
 
 	free_data(train_data,train_inst);
 	free_data(test_data,test_inst);
-
+	for (i=0;i<num_attr;i++) {
+		free(unique_array[i]);
+	}
+	free(unique_array);
+	free(unique_values);
+		
 	fclose(trainfp);
 	fclose(testfp);
 	return(0);
